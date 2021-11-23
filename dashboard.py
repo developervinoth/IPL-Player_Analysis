f072d540cc6f66e44dfe29a000bfac02c9d9d6dd
Vinoth Kumar Raja <47133106+developervinoth@users.noreply.github.com>
2021-11-23 14:24:39 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -22,7 +22,7 @@ ballByBall = pd.read_csv('https://raw.githubusercontent.com/developervinoth/Ipl_
 #Page Configuration
 st.set_page_config(  # Alternate names: setup_page, page, layout
 	layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
-	initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
+	initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
 	page_title=None,  # String or None. Strings get appended with "• Streamlit". 
 	page_icon=None,  # String, anything supported by st.image, or None.
 )
